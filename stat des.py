ba89fb1792678769e50288bf3a1472dc620aa311
jhajjar <jules.hajjar@ensae.fr>
2024-12-11 17:32:32 +0000

    début stat des

--- stat des.py
+++ stat des.py
@@ -4,6 +4,10 @@ fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspclou
 MY_BUCKET = "jhajjar"
 FILE_PATH_S3 = f"{MY_BUCKET}/Diffusion/listings.csv"
 with fs.open(FILE_PATH_S3, "r") as file_in:
-    df = pd.read_csv(file_in)
+    listings = pd.read_csv(file_in)
 
-print(df.head())
+listings.dropna(inplace=True)
+nv_noms = {'hektor-fmpconsults-474' : 'ID', 'Appartement' : 'Type', '3' : 'Number_of_rooms', '75001 Paris 1er (Vendôme)' : 'localisation', '61' : 'sqm2','847 600 €' : 'price'}
+listings.rename(columns= nv_noms, inplace=True)
+listings = listings.drop(['ID','localisation',axis=1])
+print(listings.info())
