818ee5ed6ac1330bc6fb9d872a7238a7a08e6c3f
jhajjar <jules.hajjar@ensae.fr>
2024-12-27 09:42:47 +0000

    test merge

--- stockage.py
+++ stockage.py
@@ -4,7 +4,7 @@ fs = s3fs.S3FileSystem(client_kwargs={"endpoint_url": "https://minio.lab.sspclou
 MY_BUCKET = "jhajjar"
 target_path = f"{MY_BUCKET}/Diffusion/"
 try:
-    fs.put("/home/onyxia/work/Projet_datascience_ensae/listings.csv", target_path)
+    fs.put("/home/onyxia/work/Projet_datascience_ensae/post_processed_listings.csv", target_path)
     print(f"File uploaded to {target_path}")
 except Exception as e:
     print(f"Error uploading file: {e}")
